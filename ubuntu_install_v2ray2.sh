ff5545cbc38aa442233ba774f351f618eaf6f894
Matin Habibi <itsmatinhimself@gmail.com>
2022-09-23 22:25:24 +0330

    Update ubuntu_install_v2ray2.sh

--- ubuntu_install_v2ray2.sh
+++ ubuntu_install_v2ray2.sh
@@ -125,7 +125,8 @@ function preinstall()
 function installV2ray()
 {
     echo 安装v2ray...
-    bash <(curl -L -s https://install.direct/go.sh)
+    bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
+
 
     if [ ! -f /etc/v2ray/config.json ]; then
         echo "安装失败，请到 https://www.hijk.pw 网站反馈"
